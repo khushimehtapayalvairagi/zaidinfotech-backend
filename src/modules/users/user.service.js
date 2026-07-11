08ee2f0bee68f3fb8745291bc8e2ea0f47dc29f1
Khushi Mehta <khushimehta82339@gmail.com>
2026-07-11 22:45:41 +0530

    first commit

--- src/modules/users/user.service.js
+++ src/modules/users/user.service.js
@@ -143,3 +143,17 @@ export const updateCustomerProfile = async (
 
   return user;
 };
+// ===============================
+// Get Logged In User Profile
+// ===============================
+export const getProfile = async (userId) => {
+
+  const user = await User.findById(userId)
+    .select("-password");
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return user;
+};
