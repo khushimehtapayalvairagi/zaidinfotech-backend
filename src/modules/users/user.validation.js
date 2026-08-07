606f9319b0fb26152fe7dd00764537beb201ac51
Khushi Mehta <khushimehta82339@gmail.com>
2026-08-07 14:02:11 +0530

    first commit

--- src/modules/users/user.validation.js
+++ src/modules/users/user.validation.js
@@ -2,15 +2,12 @@ import { z } from "zod";
 
 export const registerSchema = z.object({
   firstName: z
-    .string()
-    .min(2, "First name must be at least 2 characters")
-    .max(30),
+    .string(),
+    
 
   lastName: z
-    .string()
-    .min(2, "Last name must be at least 2 characters")
-    .max(30),
-
+    .string(),
+   
   email: z
     .string()
     .email("Invalid email address"),
@@ -20,12 +17,8 @@ export const registerSchema = z.object({
     .regex(/^[6-9]\d{9}$/, "Invalid phone number"),
 
   password: z
-    .string()
-    .min(8, "Password must be at least 8 characters")
-    .regex(/[A-Z]/, "Must contain one uppercase letter")
-    .regex(/[a-z]/, "Must contain one lowercase letter")
-    .regex(/[0-9]/, "Must contain one number")
-    .regex(/[@$!%*?&#]/, "Must contain one special character"),
+    .string(),
+   
 
   // role: z.enum([
   //   "SUPER_ADMIN",
@@ -45,6 +38,7 @@ export const registerSchema = z.object({
     "TECHNICIAN",
     "INVENTORY",
     "ACCOUNTANT",
+    "CUSTOMER",
     "OTHER"
 ]).optional(),
 
@@ -89,6 +83,7 @@ export const createUserSchema = z.object({
     "TECHNICIAN",
     "INVENTORY",
     "ACCOUNTANT",
+     "CUSTOMER",
   ]).optional(),
 
   department: z.enum([
